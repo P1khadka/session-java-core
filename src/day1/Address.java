7e16ebb63bb1755a3431f27cb21f18d9315850e4
Prakash Khadka <prakashkhadka@Prakashs-MacBook-Air.local>
2023-01-30 23:08:06 -0800

    states has been added

---
+++ src/day1/Address.java
@@ -0,0 +1,11 @@
+package day1;
+
+public class Address {
+    int streetNumber;
+    String streetName;
+    String city;
+    String state;
+    String country;
+    int zipCode;
+
+}
---
+++ src/day1/BankAccount.java
@@ -0,0 +1,15 @@
+package day1;
+
+public class BankAccount {
+    String accountHolderName;
+    String accountNumber;
+    String accountType;
+    String accountBalance;
+    Address address;
+    long socialSecurityNumber;
+    String openingBranch;
+    String bankName;
+
+
+
+}
